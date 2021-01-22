8fdac096ee0f8eda38650faa0423c745974752a0
adrian7bomba <ematasks@gmail.com>
2021-01-22 10:12:48 +0100

    sprawdzenieliczby2

---
+++ main.js
@@ -0,0 +1,16 @@
+let numberOne = 16;
+let numberTwo = 4;
+
+function name(numberOne, numberTwo) {
+    let result = numberOne === 20
+    return result
+}
+
+function name2(numberOne, numberTwo) {
+    let result = numberOne +numberTwo <=20
+    return result
+    
+}
+console.log(name(numberOne, numberTwo));
+
+console.log(name2(numberOne, numberTwo));
